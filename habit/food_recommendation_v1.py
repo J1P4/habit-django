cfc5f31385e7158011a868681e6d73a4f5dd263f
Kang Woobin <woobin929@dgu.ac.kr>
2024-04-30 14:46:18 +0900

    fix : AI 리턴값 변경

--- habit/food_recommendation_v1.py
+++ habit/food_recommendation_v1.py
@@ -89,5 +89,16 @@ class food_recommendation():
     last_list = first_list + second_list
     last_list = list(set(last_list))
     # print(last_list)
-    return last_list
+    
+    category_info_list = []
+    for category_num in last_list:
+        category_row = self.wweia_food_categories[self.wweia_food_categories['NO'] == category_num].iloc[0]
+        category_dict = {
+            "foodId": int(category_row['NO']),
+            "name": category_row['식품명'],
+            "category": category_row['식품상세분류']
+        }
+        category_info_list.append(category_dict)
+    
+    return category_info_list
 
